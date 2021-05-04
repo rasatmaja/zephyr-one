c744b93c54d29c8178328e7fc766dcd59b94c08f
rasatmaja <rasioatmaja29@gmail.com>
2021-05-04 22:29:00 +0700

    Create hello world handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -5,5 +5,8 @@ import "github.com/gofiber/fiber/v2"
 // Start is a function to start server
 func Start() {
 	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello from zephyr one")
+	})
 	app.Listen(":3090")
 }
