01923739b30003433e8768158113184d22762b55
rasatmaja <rasioatmaja29@gmail.com>
2021-05-05 23:19:55 +0700

    Refactor server code

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,11 +2,23 @@ package server
 
 import "github.com/gofiber/fiber/v2"
 
+// App is struct that define server, repo, and all component app needs
+type App struct {
+	server *fiber.App
+}
+
+// New is a function to initialize sever and its component
+func New() *App {
+	server := fiber.New()
+	return &App{
+		server: server,
+	}
+}
+
 // Start is a function to start server
-func Start() {
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
+func (a *App) Start() {
+	a.server.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello from zephyr one")
 	})
-	app.Listen(":3090")
+	a.server.Listen(":3090")
 }
