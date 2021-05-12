d219b9b386e60d974111b852149441fc14dd7bda
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-05-12 15:56:05 +0700

    feat(handler): create sub-log for a function (#81)
    See the issue for details
    Refactor how to initialize log for every package #72
    Refs #72

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -20,6 +20,7 @@ func New() *Endpoint {
 
 // HelloWorld ...
 func (e *Endpoint) HelloWorld(c *fiber.Ctx) error {
-	e.log.Trace().Msg("Endpoint Hello World Hit")
+	fLog := e.log.With().Str("go", "handle").Str("func", "HelloWorld").Logger()
+	fLog.Trace().Msg("Endpoint Hello World Hit")
 	return c.SendString("Hello World")
 }
