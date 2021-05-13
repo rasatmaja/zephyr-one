8b2281c366fe49918f2bdc0f96f3b7c407691a32
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-05-13 22:45:57 +0700

    refactor(logger): implement singleton using sync package (#84)
    See the issue for details
    Refactor how to initialize log for every package
    Refs #72

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -2,24 +2,26 @@ package logger
 
 import (
 	"os"
+	"sync"
 
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 var instance *Logger
+var singeton sync.Once
 
 // Logger is a trust to define logger object
 type Logger struct{ *zerolog.Logger }
 
 // New is a function to initialize zap logger
 func New() *Logger {
-	if instance == nil {
+	singeton.Do(func() {
 		// setup logger
 		logger := log.Logger
 		logger = logger.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
 		instance = &Logger{&logger}
-	}
+	})
 	return instance
 }
