9e664e6e237dbaeafe3ef85207b4ff9d3394fd7f
rasatmaja <rasioatmaja29@gmail.com>
2021-05-13 22:53:59 +0700

    chore(logger): logging on base url

--- internal/server/router.go
+++ internal/server/router.go
@@ -5,6 +5,8 @@ import "github.com/gofiber/fiber/v2"
 // InitializeRoute ...
 func (a *App) InitializeRoute() {
 	a.server.Get("/", func(c *fiber.Ctx) error {
+		fLog := a.logger.With().Str("func", "InitializeRoute").Str("go", "router").Logger()
+		fLog.Trace().Msg("Base URL Hit")
 		return c.SendString("Hello from zephyr one")
 	})
 
