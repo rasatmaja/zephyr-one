ef1ad26199bfdb663538df1f5ce20c7a8bb23cd6
rasatmaja <rasioatmaja29@gmail.com>
2021-05-22 19:26:59 +0700

    feat(logger): implement config

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"sync"
 
+	"github.com/rasatmaja/zephyr-one/internal/config"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -17,6 +18,9 @@ type Logger struct{ *zerolog.Logger }
 // New is a function to initialize zap logger
 func New() *Logger {
 	singleton.Do(func() {
+		// setup config env
+		_ = config.LoadENV()
+
 		// setup logger
 		logger := log.Logger
 		logger = logger.Output(zerolog.ConsoleWriter{Out: os.Stderr})
