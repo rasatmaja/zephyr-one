2d0fe2747b6d06586d26673564cbb42a59c86c2b
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-05-22 23:58:06 +0700

    test(logger): unit testing and benchmark on logger (#109)

---
+++ internal/logger/logger_test.go
@@ -0,0 +1,24 @@
+package logger
+
+import (
+	"os"
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		os.Setenv("LOG_LEVEL", "trace")
+		loggerTest := New()
+		if instance == nil {
+			t.Errorf("expecting instance not nil, got %v", loggerTest)
+			t.Fail()
+		}
+	})
+
+}
+
+func BenchmarkLogger(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		New()
+	}
+}
