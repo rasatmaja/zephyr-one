8722e4d7b9be07ff5a511d895aa4b681fa561f2f
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-05-26 21:25:32 +0700

    feat(error): 404 not found handler (#117)
    See the issue for details
    404 Default Response 
    Refs #111

--- internal/server/router.go
+++ internal/server/router.go
@@ -1,6 +1,10 @@
 package server
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 // InitializeRoute ...
 func (a *App) InitializeRoute() {
@@ -11,4 +15,12 @@ func (a *App) InitializeRoute() {
 	})
 
 	a.server.Get("/hello", a.handler.HelloWorld)
+
+	a.server.Use(func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusNotFound).JSON(
+			fiber.Map{
+				"message": fmt.Sprintf("URL [%s] not found", c.Path()),
+			},
+		)
+	})
 }
