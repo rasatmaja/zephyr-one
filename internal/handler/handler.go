3e6fcbc7320f302e3ceb1bc07007b5f088447088
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-05-29 17:18:36 +0700

    refactor(handler): move route not found from middleware to handler (#124)
    See the issue for details
    Setup Middleware 
    Refs #118

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/rasatmaja/zephyr-one/internal/logger"
 )
@@ -23,4 +25,13 @@ func (e *Endpoint) HelloWorld(c *fiber.Ctx) error {
 	fLog := e.log.With().Str("go", "handler").Str("func", "HelloWorld").Logger()
 	fLog.Trace().Msg("Endpoint Hello World Hit")
 	return c.SendString("Hello World")
+}
+
+// PageNotfound is a handler to handle undefined route
+func (e *Endpoint) PageNotfound(c *fiber.Ctx) error {
+	return c.Status(fiber.StatusNotFound).JSON(
+		fiber.Map{
+			"message": fmt.Sprintf("URL [%s] not found", c.Path()),
+		},
+	)
 }
