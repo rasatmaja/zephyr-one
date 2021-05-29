c650c69ca8a379428a54c884a62f4a07361fe7be
rasatmaja <rasioatmaja29@gmail.com>
2021-05-29 17:17:07 +0700

    refactor(handler): move route not found from middleware to handler

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -26,19 +26,6 @@ func New(app *fiber.App) *App {
 // InitializeMiddleware is a function to register and initialize middleware func
 func (mdlwr *App) InitializeMiddleware() {
 	mdlwr.RequestID()
-	//mdlwr.PageNotfound()
-}
-
-// PageNotfound is a function to initialize 404 page not found page as a midleware
-func (mdlwr *App) PageNotfound() {
-	fmt.Println("[ MDWR ] Initialize 404 page notfound middleware")
-	mdlwr.Use(func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusNotFound).JSON(
-			fiber.Map{
-				"message": fmt.Sprintf("URL [%s] not found", c.Path()),
-			},
-		)
-	})
 }
 
 // RequestID is a function to initialize request id for http header as a midleware
--- internal/server/router.go
+++ internal/server/router.go
@@ -6,6 +6,8 @@ import (
 
 // InitializeRoute ...
 func (a *App) InitializeRoute() {
+	defer a.server.Use(a.handler.PageNotfound)
+
 	a.server.Get("/", func(c *fiber.Ctx) error {
 		fLog := a.logger.With().Str("func", "InitializeRoute").Str("go", "router").Logger()
 		fLog.Trace().Msg("Base URL Hit")
