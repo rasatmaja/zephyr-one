ce8d7d984a2151c50afd69ce42cd2dc239f813cf
rasatmaja <rasioatmaja29@gmail.com>
2021-05-31 15:06:07 +0700

    feat(middleware): trasaction ID middleware

--- internal/server/router.go
+++ internal/server/router.go
@@ -6,12 +6,19 @@ import (
 
 // InitializeRoute ...
 func (a *App) InitializeRoute() {
+	fLog := a.logger.With().Str("func", "InitializeRoute").Str("go", "router").Logger()
 	defer a.server.Use(a.handler.PageNotfound)
 
 	a.server.Get("/", func(c *fiber.Ctx) error {
-		fLog := a.logger.With().Str("func", "InitializeRoute").Str("go", "router").Logger()
 		fLog.Trace().Msg("Base URL Hit")
 		return c.SendString("Hello from zephyr one")
+	})
+
+	a.server.Post("/", func(c *fiber.Ctx) error {
+		fLog.Trace().Msg("Base URL POST Hit")
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "Hello from Zephyr One",
+		})
 	})
 
 	a.server.Get("/hello", a.handler.HelloWorld)
