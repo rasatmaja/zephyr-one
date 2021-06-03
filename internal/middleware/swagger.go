9dd483a49b5e42d4bec950e87503b500fd31d26e
rasatmaja <rasioatmaja29@gmail.com>
2021-06-03 22:00:23 +0700

    chore(swagger): change path from docs/ to /docs

--- internal/middleware/swagger.go
+++ internal/middleware/swagger.go
@@ -12,17 +12,16 @@ import (
 //go:embed swagger/*
 var swaggerDir embed.FS
 
-// SwaggerUI ...
+// SwaggerUI is a middleware to server Swagger UI
 func (mdlwr *App) SwaggerUI() {
 	fmt.Println("[ MDWR ] Initialize Swagger UI")
-	mdlwr.server.Use("docs/", filesystem.New(filesystem.Config{
+	mdlwr.server.Use("/docs", filesystem.New(filesystem.Config{
 		Root:       http.FS(swaggerDir),
 		PathPrefix: "swagger",
 		Browse:     true,
-		Index:      "index.html",
 	}))
 
-	mdlwr.server.Get("docs/", func(c *fiber.Ctx) error {
+	mdlwr.server.Get("/docs", func(c *fiber.Ctx) error {
 		slashPresent := c.Path()[len(c.Path())-1:] == "/"
 		if slashPresent {
 			return c.Redirect("index.html")
