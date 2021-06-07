ba8ab79f91242a700f8a43a74813b6769f3d02d6
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-06-07 14:03:23 +0700

    feat(server): tls config & self-signed certificate (#141)
    See the issue for details
    Setup SSL on development server 
    Refs #137

---
+++ internal/server/certificate.go
@@ -0,0 +1,81 @@
+package server
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"fmt"
+	"math/big"
+	"os"
+	"time"
+)
+
+// Certificate ..
+type Certificate struct {
+	CertPath string
+	KeyPath  string
+}
+
+// GenerateSelfSignedCertificates ...
+func (a *App) GenerateSelfSignedCertificates() *Certificate {
+	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to generate private key: %v", err))
+	}
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to generate serial number: %v", err))
+	}
+
+	cert := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"Localhost Corp."},
+		},
+		DNSNames:  []string{"localhost"},
+		NotBefore: time.Now(),
+		NotAfter:  time.Now().Add(3 * time.Hour),
+
+		KeyUsage:              x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	// Generate certificate file
+	derBytes, err := x509.CreateCertificate(rand.Reader, &cert, &cert, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create certificate: %v", err))
+	}
+
+	pemCert := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+	if pemCert == nil {
+		panic("Failed to encode certificate to PEM")
+	}
+
+	if err := os.WriteFile("cert.pem", pemCert, 0644); err != nil {
+		panic(err)
+	}
+
+	// Generate key file
+	privBytes, err := x509.MarshalPKCS8PrivateKey(privateKey)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to marshal private key: %v", err))
+	}
+	pemKey := pem.EncodeToMemory(&pem.Block{Type: "PRIVATE KEY", Bytes: privBytes})
+	if pemKey == nil {
+		panic("Failed to encode key to PEM")
+	}
+	if err := os.WriteFile("key.pem", pemKey, 0600); err != nil {
+		panic(err)
+	}
+
+	return &Certificate{
+		CertPath: "cert.pem",
+		KeyPath:  "key.pem",
+	}
+}
