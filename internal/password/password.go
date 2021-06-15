00ba0701e02458df5e2759bc22fd330f6b2e2931
rasatmaja <rasioatmaja29@gmail.com>
2021-06-15 21:52:21 +0700

    feat(password): define password interface

---
+++ internal/password/password.go
@@ -0,0 +1,7 @@
+package password
+
+// IPassword is a interface to define password functions
+type IPassword interface {
+	Hash(plain string) (string, error)
+	Compare(plain, hash string) (bool, error)
+}
