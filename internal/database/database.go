4ae101b2514a9b643031e8982ab9ff44e73cda87
rasatmaja <rasioatmaja29@gmail.com>
2021-06-22 23:38:15 +0700

    refactor(database): cretae database factory

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,22 @@
 package database
+
+import (
+	"fmt"
+
+	"github.com/rasatmaja/zephyr-one/internal/config"
+	"github.com/rasatmaja/zephyr-one/internal/database/postgresql"
+	"github.com/rasatmaja/zephyr-one/internal/database/repository"
+)
+
+// Factory is a function to build database connection pool
+// bassed on database type
+func Factory() repository.IRepository {
+	env := config.LoadENV()
+	switch env.DatabaseType {
+	case "POSTGRESQL":
+		return postgresql.New()
+	default:
+		fmt.Println("[ DTBS ] Database Type unrecognize, using default database: POSTGRESQL")
+		return postgresql.New()
+	}
+}
