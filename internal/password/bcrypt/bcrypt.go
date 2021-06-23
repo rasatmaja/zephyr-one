9e53980cb077173a966eb6a9b2c47f87ab48f8c9
rasatmaja <rasioatmaja29@gmail.com>
2021-06-23 14:38:10 +0700

    feat(password): implement bcrypt compare password

--- internal/password/bcrypt/bcrypt.go
+++ internal/password/bcrypt/bcrypt.go
@@ -7,7 +7,7 @@ type Bcrypt struct {
 	cost int
 }
 
-// New ...
+// New is a function to initialize bcrypt
 func New() *Bcrypt {
 	bcrypt := &Bcrypt{
 		cost: bcrypt.DefaultCost,
@@ -19,4 +19,10 @@ func New() *Bcrypt {
 func (b *Bcrypt) Hash(plain string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(plain), b.cost)
 	return string(hash), err
+}
+
+// Compare is a function to compare plain string with hashed string
+func (b *Bcrypt) Compare(plain, hash string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(plain))
+	return err == nil, err
 }
