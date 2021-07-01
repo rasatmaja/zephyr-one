679ff533f74a1da9cb6b2c380aac341cd7ab18a7
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-07-01 21:18:40 +0700

    refactor(registration): improve repository (#204)
    See the issue for details
    - Registration #190 
    - Create registration flow #199

--- internal/database/postgresql/auth.go
+++ internal/database/postgresql/auth.go
@@ -3,24 +3,25 @@ package postgresql
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/rasatmaja/zephyr-one/internal/database/models"
 )
 
 // CreateAuth is a function to insert data into table auth
 func (qry *Queries) CreateAuth(ctx context.Context, username, passphrase string) (*models.Auth, error) {
+	// build auth model
+	auth := &models.Auth{
+		ID:         uuid.NewString(),
+		Username:   username,
+		Passphrase: passphrase,
+	}
+
 	// insert username and passphrase into database
-	_, err := qry.DB.ExecContext(ctx, "INSERT INTO auth(username, passphrase) VALUES($1,$2)", username, passphrase)
+	_, err := qry.DB.ExecContext(ctx, "INSERT INTO auth(id, username, passphrase) VALUES($1, $2, $3)", auth.ID, auth.Username, auth.Passphrase)
 
 	if err != nil {
 		return nil, err
 	}
 
-	auth := &models.Auth{}
-	// select auth data
-	row := qry.DB.QueryRowContext(ctx, "SELECT id, username, passphrase FROM auth WHERE username = $1", username)
-	err = row.Scan(&auth.ID, &auth.Username, &auth.Passphrase)
-	if err != nil {
-		return nil, err
-	}
 	return auth, nil
 }
