0c66ee4732b5d7993371c2e55afd729b7748954e
rasatmaja <rasioatmaja29@gmail.com>
2021-07-03 13:35:42 +0700

    refactor(model): create base models

--- internal/database/models/auth.go
+++ internal/database/models/auth.go
@@ -4,25 +4,10 @@ import "time"
 
 // Auth define column table auth
 type Auth struct {
+	Models
 	ID         string    `column:"id"`
 	Username   string    `column:"username"`
 	Passphrase string    `column:"passphrase"`
 	CreatedAt  time.Time `column:"created_at"`
 	UpdatedAt  time.Time `column:"updated_at"`
-
-	// additionals fields
-	columns   []string
-	fieldsptr []interface{}
-}
-
-// Columns is a method to get columns name
-func (auth *Auth) Columns() []string {
-	auth.columns = columns(auth)
-	return auth.columns
-}
-
-// Fields is a method to get fields pointer
-func (auth *Auth) Fields() []interface{} {
-	auth.fieldsptr = fields(auth)
-	return auth.fieldsptr
 }
