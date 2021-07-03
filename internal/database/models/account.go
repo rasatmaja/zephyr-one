bb85672f560418d0e375c3e3d68de18622d10957
rasatmaja <rasioatmaja29@gmail.com>
2021-07-03 12:05:11 +0700

    test(mode): unit test auth and accont info

--- internal/database/models/account.go
+++ internal/database/models/account.go
@@ -4,11 +4,11 @@ import "time"
 
 // AccountInfo define column table account_info
 type AccountInfo struct {
-	ID        string
-	Photo     string
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        string    `column:"id"`
+	Photo     string    `column:"photo_profile"`
+	Name      string    `column:"name"`
+	CreatedAt time.Time `column:"created_at"`
+	UpdatedAt time.Time `column:"updated_at"`
 	// additionals fields
 	columns   []string
 	fieldsptr []interface{}
