071d2a1c16b1a1f41d28217029e73fc462492220
rasatmaja <rasioatmaja29@gmail.com>
2021-07-09 21:11:43 +0700

    feat(token): define interface and struct

--- internal/token/token.go
+++ internal/token/token.go
@@ -1,6 +1,19 @@
 package token
 
 import (
-	// blank import jwt dependency
-	_ "github.com/gbrlsnchs/jwt/v3"
+	"context"
+
+	"github.com/gbrlsnchs/jwt/v3"
 )
+
+// IToken define interface
+type IToken interface {
+	Sign(ctx context.Context, payload Payload) error
+	Verify(ctx context.Context, token string) (error, Payload)
+}
+
+// Payload define JWT field
+type Payload struct{ jwt.Payload }
+
+// Token is a JWT struct
+type Token struct{}
