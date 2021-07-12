25a995abeb2e9c4b82486fb35175c3e69d437b9e
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-07-12 23:09:02 +0700

    test(token): unit test verify token basic (#254)
    See the issue for details
    - Unit testing #227
    - Authenticates user credential  #214

--- internal/token/basic/basic_test.go
+++ internal/token/basic/basic_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/gbrlsnchs/jwt/v3"
 	"github.com/rasatmaja/zephyr-one/internal/token/contract"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -129,4 +130,112 @@ func TestSign(t *testing.T) {
 		assert.NoError(t, err)
 		assert.NotEqual(t, 0, len(jwt))
 	})
+}
+
+func TestVerify(t *testing.T) {
+	ctx := context.Background()
+	token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJ6ZXByaHlyLW9uZSIsInN1YiI6InJhc2lvMjkiLCJleHAiOjE2Mjg2NzcwMTIsIm5iZiI6MTYyNjA4NTAxMiwiaWF0IjoxNjI2MDg1MDEyLCJqdGkiOiIwMmY5Zjc5Zi0xMWM2LTRhNmQtYjM4OC01NjRiM2RlMzc5ZDEifQ.pmo0AQVknJ8BhIZBNYFNVyNm8545FTvTVu0x1IT4lfI"
+
+	tkn := &Token{
+		Token: &contract.Token{
+			SignKey: "secret",
+		},
+		signature: nil,
+	}
+	tkn.Issuer = "test"
+
+	t.Run("error-verify", func(t *testing.T) {
+
+		// start mock
+		alg := &Mocks{}
+		alg.On("Name").Return("unknown")
+		alg.On("Size").Return(1)
+		alg.On("Verify", mock.Anything, mock.Anything).Return(fmt.Errorf("error"))
+		tkn.signature = alg
+
+		payload, err := tkn.Verify(ctx, token)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "error")
+		assert.Nil(t, payload)
+	})
+
+	t.Run("token-expired", func(t *testing.T) {
+
+		tkn.buildSignature(ctx)
+		tkn.Issuer = "test"
+
+		payload := &contract.Payload{
+			ExpirationTime: contract.TimeNow().AddDates(0, 0, -1),
+		}
+		tokenjwt, err := jwt.Sign(payload, tkn.signature)
+		payloadRes, err := tkn.Verify(ctx, string(tokenjwt))
+		assert.Error(t, err)
+		assert.EqualError(t, err, contract.ErrExp.Error())
+		assert.Nil(t, payloadRes)
+	})
+
+	t.Run("token-issued-from-future", func(t *testing.T) {
+
+		tkn.buildSignature(ctx)
+
+		payload := &contract.Payload{
+			ExpirationTime: contract.TimeNow().AddDates(0, 0, 5),
+			IssuedAt:       contract.TimeNow().AddDates(0, 0, 1),
+		}
+		tokenjwt, err := jwt.Sign(payload, tkn.signature)
+		payloadRes, err := tkn.Verify(ctx, string(tokenjwt))
+		assert.Error(t, err)
+		assert.EqualError(t, err, contract.ErrIat.Error())
+		assert.Nil(t, payloadRes)
+	})
+
+	t.Run("token-not-before", func(t *testing.T) {
+
+		tkn.buildSignature(ctx)
+
+		payload := &contract.Payload{
+			ExpirationTime: contract.TimeNow().AddDates(0, 0, 5),
+			IssuedAt:       contract.TimeNow(),
+			NotBefore:      contract.TimeNow().AddDates(0, 0, 1),
+		}
+		tokenjwt, err := jwt.Sign(payload, tkn.signature)
+		payloadRes, err := tkn.Verify(ctx, string(tokenjwt))
+		assert.Error(t, err)
+		assert.EqualError(t, err, contract.ErrNbf.Error())
+		assert.Nil(t, payloadRes)
+	})
+
+	t.Run("token-mismatch-issuer", func(t *testing.T) {
+
+		tkn.buildSignature(ctx)
+
+		payload := &contract.Payload{
+			ExpirationTime: contract.TimeNow().AddDates(0, 0, 5),
+			IssuedAt:       contract.TimeNow(),
+			NotBefore:      contract.TimeNow(),
+			Issuer:         "not-test",
+		}
+		tokenjwt, err := jwt.Sign(payload, tkn.signature)
+		payloadRes, err := tkn.Verify(ctx, string(tokenjwt))
+		assert.Error(t, err)
+		assert.EqualError(t, err, contract.ErrIss.Error())
+		assert.Nil(t, payloadRes)
+	})
+
+	t.Run("success", func(t *testing.T) {
+
+		tkn.buildSignature(ctx)
+
+		payload := &contract.Payload{
+			ExpirationTime: contract.TimeNow().AddDates(0, 0, 5),
+			IssuedAt:       contract.TimeNow(),
+			NotBefore:      contract.TimeNow(),
+			Issuer:         "test",
+		}
+		tokenjwt, err := jwt.Sign(payload, tkn.signature)
+		payloadRes, err := tkn.Verify(ctx, string(tokenjwt))
+		assert.NoError(t, err)
+		assert.NotNil(t, payloadRes)
+	})
+
 }
