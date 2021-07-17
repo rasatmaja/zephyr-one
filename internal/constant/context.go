b97bd78508e670b8afbc20232d51894f9dec09da
Rasio Ganang Atmaja <rasioatmaja29@gmail.com>
2021-07-17 21:42:20 +0700

    feat(context): set context value with jwt user id (#275)
    See the issue for details
    - Inject user UUID inside context #271
    - User Contact #259

---
+++ internal/constant/context.go
@@ -0,0 +1,13 @@
+package constant
+
+// AuthIDContext define field context value
+const AuthIDContext = "USER_ID"
+
+// XRequestIDHTTPHeader define request ID HTTP header
+const XRequestIDHTTPHeader = "X-Request-Id"
+
+// XtransactionIDHTTPHeader define transaction ID HTTP header
+const XtransactionIDHTTPHeader = "X-Transaction-Id"
+
+// AuthHeader define Authorization http header
+const AuthHeader = "Authorization"
