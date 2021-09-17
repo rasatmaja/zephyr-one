97a39ffe55f02e23a3a00133d7c86465515734a9
Rasio Atmaja <rasioatmaja29@gmail.com>
2021-09-17 21:46:20 +0700

    feat(info): endpoint to retrive user info (#347)
    See the issue for details
    - User Info #337
    - Add endpoint to get user info #338

--- internal/database/models/account.go
+++ internal/database/models/account.go
@@ -1,6 +1,9 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 // AccountInfo define column table account_info
 type AccountInfo struct {
@@ -10,4 +13,18 @@ type AccountInfo struct {
 	Name      string    `column:"name"`
 	CreatedAt time.Time `column:"created_at"`
 	UpdatedAt time.Time `column:"updated_at"`
+}
+
+// MarshalJSON is a function to marshaling field account info
+func (c *AccountInfo) MarshalJSON() ([]byte, error) {
+	ctc := struct {
+		Name             string `json:"name"`
+		Photo            string `json:"photo"`
+		RegistrationDate string `json:"registration_date"`
+	}{
+		Name:             c.Name,
+		Photo:            c.Photo,
+		RegistrationDate: c.CreatedAt.Format("Monday 02, January 2006 15:04 MST"),
+	}
+	return json.Marshal(ctc)
 }
