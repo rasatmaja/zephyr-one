25ab377c65a9ccf1286c699babd301673aa9a3b5
rasatmaja <rasioatmaja29@gmail.com>
2021-11-09 09:37:34 +0700

    refactor(config): move to mura

--- go.mod
+++ go.mod
@@ -5,11 +5,13 @@ go 1.16
 require (
 	github.com/gbrlsnchs/jwt/v3 v3.0.1
 	github.com/gofiber/fiber/v2 v2.15.0
+	github.com/google/go-cmp v0.5.5 // indirect
 	github.com/google/uuid v1.3.0
 	github.com/jackc/pgx/v4 v4.13.0
+	github.com/rasatmaja/mura/v2 v2.1.1
 	github.com/rs/zerolog v1.23.0
-	github.com/spf13/viper v1.8.1
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -2,8 +2,10 @@ package config
 
 import (
 	"fmt"
-	"os"
+	"log"
 	"sync"
+
+	"github.com/rasatmaja/mura/v2"
 )
 
 var instance *ENV
@@ -19,39 +21,39 @@ type Config struct {
 // ENV is a stuct to hold all environemnt variable for this app
 type ENV struct {
 	// Server
-	ServerHost       string `mapstructure:"SERVER_HOST" default:"localhost"`
-	ServerPort       int    `mapstructure:"SERVER_PORT" default:"2929"`
-	ServerReadTO     int    `mapstructure:"SERVER_READ_TIMEOUT" default:"10"`
-	ServerWriteTO    int    `mapstructure:"SERVER_WRITE_TIMEOUT" default:"10"`
-	ServerIdleTO     int    `mapstructure:"SERVER_IDLE_TIMEOUT" default:"10"`
-	ServerProduction bool   `mapstructure:"SERVER_PRODUCTION" default:"false"`
+	ServerHost       string `env:"SERVER_HOST" default:"localhost"`
+	ServerPort       int    `env:"SERVER_PORT" default:"2929"`
+	ServerReadTO     int    `env:"SERVER_READ_TIMEOUT" default:"10"`
+	ServerWriteTO    int    `env:"SERVER_WRITE_TIMEOUT" default:"10"`
+	ServerIdleTO     int    `env:"SERVER_IDLE_TIMEOUT" default:"10"`
+	ServerProduction bool   `env:"SERVER_PRODUCTION" default:"false"`
 
 	//TLS
-	TLS bool `mapstructure:"TLS" default:"false"`
+	TLS bool `env:"TLS" default:"false"`
 
 	// LOG
-	LogLevel  string `mapstructure:"LOG_LEVEL" default:"ERROR"` // TRACE, DEBUG, INFO, ERROR
-	LogOutput string `mapstructure:"LOG_OUTPUT" default:"CMD"`  // CMD, JSON
+	LogLevel  string `env:"LOG_LEVEL" default:"ERROR"` // TRACE, DEBUG, INFO, ERROR
+	LogOutput string `env:"LOG_OUTPUT" default:"CMD"`  // CMD, JSON
 
 	// DABATASE
-	DatabaseType        string `mapstructure:"DB_TYPE" default:"POSTGRESQL"`
-	DatabaseMaxIDLE     int    `mapstructure:"DB_MAX_IDLE" default:"25"`     // 25
-	DatabaseMaxOpen     int    `mapstructure:"DB_MAX_OPEN" default:"25"`     // 25
-	DatabaseMaxLifetime int    `mapstructure:"DB_MAX_LIFETIME" default:"10"` // IN MINUTES
+	DatabaseType        string `env:"DB_TYPE" default:"POSTGRESQL"`
+	DatabaseMaxIDLE     int    `env:"DB_MAX_IDLE" default:"25"`     // 25
+	DatabaseMaxOpen     int    `env:"DB_MAX_OPEN" default:"25"`     // 25
+	DatabaseMaxLifetime int    `env:"DB_MAX_LIFETIME" default:"10"` // IN MINUTES
 
 	// DATABASE POSTGRESQL
-	DBPostgresHost     string `mapstructure:"DB_PG_HOST" default:"localhost"`
-	DBPostgresPort     int    `mapstructure:"DB_PG_PORT" default:"5432"`
-	DBPostgresUsername string `mapstructure:"DB_PG_USER" default:"root"`
-	DBPostgresPassword string `mapstructure:"DB_PG_PASSWORD" default:"root"`
-	DBPostgresDatabase string `mapstructure:"DB_PG_DATABASE" default:"zepheryone"`
-	DBPostgresSSLMode  string `mapstructure:"DB_PG_SSLMODE" default:"disable"`
+	DBPostgresHost     string `env:"DB_PG_HOST" default:"localhost"`
+	DBPostgresPort     int    `env:"DB_PG_PORT" default:"5432"`
+	DBPostgresUsername string `env:"DB_PG_USER" default:"root"`
+	DBPostgresPassword string `env:"DB_PG_PASSWORD" default:"root"`
+	DBPostgresDatabase string `env:"DB_PG_DATABASE" default:"zepheryone"`
+	DBPostgresSSLMode  string `env:"DB_PG_SSLMODE" default:"disable"`
 
 	// TOKEN JWT
-	TokenType    string `mapstructure:"TOKEN_TYPE" default:"BASIC"`
-	TokenSignKey string `mapstructure:"TOKEN_SIGN_KEY" default:"secret"`
-	TokenSignAlg string `mapstructure:"TOKEN_SIGN_ALG" default:"HS256"`
-	TokenIssuer  string `mapstructure:"TOKEN_ISSUER" default:"zephery-one"`
+	TokenType    string `env:"TOKEN_TYPE" default:"BASIC"`
+	TokenSignKey string `env:"TOKEN_SIGN_KEY" default:"secret"`
+	TokenSignAlg string `env:"TOKEN_SIGN_ALG" default:"HS256"`
+	TokenIssuer  string `env:"TOKEN_ISSUER" default:"zephery-one"`
 }
 
 // LoadENV ...
@@ -59,9 +61,7 @@ func LoadENV() *ENV {
 	singleton.Do(func() {
 		fmt.Println("[ CNFG ] Starting ENV config ...")
 		config := &Config{
-			Filename: "app",
-			Type:     "env",
-			Path:     ".",
+			Path: ".env",
 		}
 		instance = config.BuildENV()
 	})
@@ -70,30 +70,12 @@ func LoadENV() *ENV {
 
 // BuildENV ...
 func (cfg *Config) BuildENV() *ENV {
-	env := &ENV{}
-
-	vpr := GetViper()
-	vpr.AddConfigPath(cfg.Path)
-	vpr.SetConfigName(cfg.Filename)
-	vpr.SetConfigType(cfg.Type)
-
-	vpr.FillDefault(env)
-	vpr.AutomaticEnv()
-	vpr.BindEnvs(env)
-
-	if err := vpr.ReadInConfig(); err != nil {
-		if vpr.IsFileNotFoundError(err) {
-			pwd, _ := os.Getwd()
-			fmt.Printf("[ CNFG ] File app.env not found on '%s',\n", pwd)
-			fmt.Println("[ CNFG ] Using system variable")
-		} else {
-			panic(err)
-		}
-	}
-
-	if err := vpr.Unmarshal(&env); err != nil {
-		panic(err)
-	}
-
+	env := new(ENV)
+	mura.SetENVPath(cfg.Path)
+	mura.Unmarshal(env)
 	return env
+}
+
+func init() {
+	log.SetPrefix("")
 }
