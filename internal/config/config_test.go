6feda39bfcae42a0ef9a890e056b0c46d1dc6ce4
Rasio Atmaja <rasioatmaja29@gmail.com>
2021-11-09 09:57:08 +0700

    refactor(config): move to mura (#352)
    See the issue for details
    - Migration env config to mura #326

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -8,47 +8,13 @@ import (
 func TestLoadENV(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
 		defer func() {
+			os.Remove(".env")
 			if r := recover(); r != nil {
 				t.Errorf("The code did panic")
 				t.Fail()
 			}
 		}()
+		os.WriteFile(".env", []byte("SERVER_PRODUCTION=localhost"), 0600)
 		LoadENV()
-	})
-}
-
-func TestBuildENV(t *testing.T) {
-	t.Run("error-read-config", func(t *testing.T) {
-		defer func() {
-			os.Remove("app")
-			if r := recover(); r == nil {
-				t.Errorf("The code did panic")
-				t.Fail()
-			}
-		}()
-		os.WriteFile("app", []byte(""), 0600)
-		config := &Config{
-			Filename: "app",
-			Type:     "unknown",
-			Path:     ".",
-		}
-		config.BuildENV()
-	})
-
-	t.Run("error-unmarshall", func(t *testing.T) {
-		defer func() {
-			os.Remove("app")
-			if r := recover(); r == nil {
-				t.Errorf("The code did panic")
-				t.Fail()
-			}
-		}()
-		os.WriteFile("app", []byte("SERVER_PRODUCTION=localhost"), 0600)
-		config := &Config{
-			Filename: "app",
-			Type:     "env",
-			Path:     ".",
-		}
-		config.BuildENV()
 	})
 }
